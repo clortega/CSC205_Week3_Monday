98f658d8af305eb968718e2d822592756e617b73
clortega <carlos.l.ortega@outlook.com>
2023-11-06 15:28:43 -0800

    put everything in its own method

--- Main.java
+++ Main.java
@@ -6,37 +6,10 @@
 // enum Cart {SHOPPING, TAG, RETURN, SAVEDFORLATER}
 
 public class Main {
-
     public static void main(String[] args) {
         SeasonsLogic();
-
-        Student student1 = new Student(); //student1 refers to a specific instance or version of the Student class
-        student1.setFirstName("Eric");
-        student1.setLastName("Student");
-        student1.GPA = 3.0f;
-        student1.Major = Major.ART;
-
-        Student student2 = new Student();
-        // Student student3 = student1;
-        // student3.FirstName = "Melissa"; //because student3 points to the same location as student1, it overrides "Eric" as the first name.
-        
-        Car car1 = new Car();
-        car1.Mileage = 50000;
-        car1.BodyStyle = BodyStyle.SEDAN;
-        car1.CarMake = Make.FORD;
-        Car car2 = new Car();
-        car2.Mileage = 12000;
-        car2.BodyStyle = BodyStyle.COUPE;
-        car2.CarMake = Make.TOYOTA;
-        Car car3 = new Car();
-        car3.Mileage = 60000;
-        car3.BodyStyle = BodyStyle.TRUCK;
-        car3.CarMake = Make.GMC;
-
-
-        System.out.println(student1.getFirstName() + " " + student1.getLastName());
-        System.out.println(student2);
-        // System.out.println(student3.FirstName);
+        CarLogic();
+        StudentLogic();
     }
 
     public static void SeasonsLogic() {
@@ -53,5 +26,36 @@ public class Main {
         else {
             System.err.println("Fall and Summer are both okay");
         }
+    }
+
+    public static void CarLogic() {
+        Car car1 = new Car();
+        car1.Mileage = 50000;
+        car1.BodyStyle = BodyStyle.SEDAN;
+        car1.CarMake = Make.FORD;
+        Car car2 = new Car();
+        car2.Mileage = 12000;
+        car2.BodyStyle = BodyStyle.COUPE;
+        car2.CarMake = Make.TOYOTA;
+        Car car3 = new Car();
+        car3.Mileage = 60000;
+        car3.BodyStyle = BodyStyle.TRUCK;
+        car3.CarMake = Make.GMC;
+    }
+    
+    public static void StudentLogic() {
+        Student student1 = new Student(); //student1 refers to a specific instance or version of the Student class
+        student1.setFirstName("Eric");
+        student1.setLastName("Student");
+        student1.GPA = 3.0f;
+        student1.Major = Major.ART;
+
+        Student student2 = new Student();
+        // Student student3 = student1;
+        // student3.FirstName = "Melissa"; //because student3 points to the same location as student1, it overrides "Eric" as the first name.
+        
+        System.out.println(student1.getFirstName() + " " + student1.getLastName());
+        System.out.println(student2);
+        // System.out.println(student3.FirstName);
     }
 }
