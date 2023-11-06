ae2837417a537fa08d32ff69904215c65ca47962
clortega <carlos.l.ortega@outlook.com>
2023-11-06 14:41:41 -0800

    "We added Car"

---
+++ Car.java
@@ -0,0 +1,9 @@
+enum Make {FORD, DODGE, GMC, MAZDA, TOYOTA}
+enum BodyStyle {SEDAN, COUPE, TRUCK, SUV, VAN}
+
+public class Car {
+    Make CarMake; //Again the first refers to data type (enum), the second to the property
+    double Mileage;
+    BodyStyle BodyStyle;
+    
+}
